e7ade01bc0eaa078c65d08ef15d017732a528958
satyamkota <satyamkota9595@gmail.com>
2021-10-31 00:45:07 +0530

    First commit

---
+++ backend/model/product.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    "name":{type:String,required:true},
+    "brand":{type:String,required:true},
+    "rating":{type:Number,required:true},
+    "numReviews":{type:Number,required:true},
+    "image":{type:String,required:true},
+    "description":{type:String,required:true}
+});
+
+const Product = mongoose.model("Product",productSchema);
+
+module.exports = Product;
+
